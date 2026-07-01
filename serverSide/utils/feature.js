ab6432363cdc55633880b61c178c0b7cdfcb90ba
Ankitkumar Suthar <ankitsuthar683@gmail.com>
2026-07-01 17:19:59 +0530

    fix: use actual JWT_SECRET env variable instead of string literal

--- serverSide/utils/feature.js
+++ serverSide/utils/feature.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken"
 
 
 export const generateCookie = (user,res, statusCode=200,message) =>{
-    const token = jwt.sign({_id:user._id}, 'process.env.JWT_SECRET')
+    const token = jwt.sign({_id:user._id}, process.env.JWT_SECRET)
       console.log(token)
     
       res.status(201).cookie("token",token,{
